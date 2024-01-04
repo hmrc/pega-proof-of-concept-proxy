a0641255c293225213e4eeaacd6238db0c3dff87
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2024-01-04 13:54:56 +0000

    'Creating new service pega-proof-of-concept-proxy'

---
+++ app/uk/gov/hmrc/pegaproofofconceptproxy/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.pegaproofofconceptproxy.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
